0ef2ec238ddff93f2494ae593b92b4e94984dada
Lorenzo Mancini <l.mancini@cineca.it>
2021-06-29 16:07:38 +0200

    aggiunta sotto modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModuleModule} from './subModule/shared-module.module'
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SharedModuleModule,
     AppRoutingModule
   ],
   providers: [],
